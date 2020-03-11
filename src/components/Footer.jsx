643c773ec5ffeae67290dfb454a7f44ad4da5c68
elikantor <eli.kantor@outlook.com>
2020-03-11 15:45:56 -0400

    updated create survey styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,8 @@ export default class Footer extends React.Component{
             <div>
             <p> Created by Eli Kantor <br></br> 
             Front-end: React, Redux, Javascript, Chart JS, CSS, HTML, Semantic UI React, React Router<br></br>
-            Back-end: Ruby on Rails, ActionMailer, JWT, BCrypt, PostgreSQL</p>
+            Back-end: Ruby on Rails, ActionMailer, JWT, BCrypt, PostgreSQL
+            Deployment: Heroku</p>
             </div>
             </div>
         )
